bfea61e73a6c9a6c73e9ecbc50976baaf97b01ca
Chris <chris@adadevelopersacademy.org>
2016-10-27 22:14:28 -0700

    Moving from post to get

---
+++ app/controllers/homepages_controller.rb
@@ -0,0 +1,27 @@
+require "#{Rails.root}/lib/SlackApiWrapper.rb"
+
+class HomepagesController < ApplicationController
+  def index
+    @data = SlackApiWrapper.listchannels
+    if @data != nil && @data != []
+      render status: :created
+    else
+      render status: :service_unavailable
+    end
+  end
+
+  def new
+    @channel = params[:channel]
+    @channel_id = params[:id]
+  end
+
+  def create
+    result = SlackApiWrapper.sendmsg(params["channel"], params["message"])
+    if result["ok"]
+      flash[:notice] = "Message Sent"
+    else
+      flash[:notice] = "Message Failed to Send #{results['ok']}"
+    end
+    redirect_to :root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :homepages, only: [:index,  :create]
+  root to: 'homepages#index'
+  get '/new/:channel/:id' => 'homepages#new', as: 'new_message'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
