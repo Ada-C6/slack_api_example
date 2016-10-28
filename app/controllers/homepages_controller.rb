30f7e8834e17441dc86b933e0e9c026ca3bcad7c
Chris <chris@adadevelopersacademy.org>
2016-10-27 21:48:41 -0700

    added response codes.

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -20,12 +20,7 @@ class HomepagesController < ApplicationController
 
   def create
     result = SlackApiWrapper.sendmsg(params["channel"], params["message"])
-    if result["ok"]
-      resp = :created
-    else
-      resp = :service_unavailable
-    end
-    raise
-    redirect_to root_path, resp
+
+    redirect_to root_path(), resp
   end
 end
